99086f1c03ac69812e057a3522318a165f44b9f8
sosath <sosalijeronthomas@gmail.com>
2026-01-16 12:24:09 -0400

    save

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,72 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            string response = Console.ReadLine();
+            userNumber = int.Parse(response);
+            
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = ((double)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average.ToString(CultureInfo.InvariantCulture)}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+
+        int smallestPositive = int.MaxValue; 
+        bool foundPositive = false;
+
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+                foundPositive = true;
+            }
+        }
+
+        if (foundPositive)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+
+        numbers.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
