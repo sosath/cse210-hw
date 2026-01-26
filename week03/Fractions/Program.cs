161af778b1d5e166b138d93e16b62f7fb3d4dd2a
sosath <sosalijeronthomas@gmail.com>
2026-01-26 18:29:03 -0400

    fraction project

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using Fractions;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        f1.SetTop(10);
+        f1.SetBottom(2);
+        Console.WriteLine($"\nPrueba de Setters en f1: {f1.GetTop()}/{f1.GetBottom()}");
     }
 }
