efb13457b05649b0742f08a1b3da48334ad7f92a
sosath <sosalijeronthomas@gmail.com>
2026-02-14 21:46:47 -0400

    save

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double _radius;
+
+        public Circle(string color, double radius) : base(color)
+        {
+            _radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace Shapes
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            Square square1 = new Square("Red", 4);
+            shapes.Add(square1);
+
+            Rectangle rect1 = new Rectangle("Blue", 5, 10);
+            shapes.Add(rect1);
+
+            Circle circ1 = new Circle("Green", 3.5);
+            shapes.Add(circ1);
+
+            Console.WriteLine("--- List of Shapes ---");
+
+            foreach (Shape s in shapes)
+            {
+                string color = s.GetColor();
+                double area = s.GetArea();
+
+                Console.WriteLine($"Color: {color}, Area: {area:F2}");
+            }
+        }
     }
 }
